28cc35f043ff7cd93f5ce11162d62eea9feb8322
AnkitKumar <ankit.kumar@infostride.com>
2022-01-13 15:51:10 +0530

    next project

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
     public static void main(String args[]){
+        System.setProperty("webdriver.chrome.driver", "D:\\SeleniumProject\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://dev.bravvox.com/");
+        System.out.println(driver.getTitle());
+        driver.quit();
 
 
 
