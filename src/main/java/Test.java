868bf779f1d58e5c0f4d8e7b26f6a1dbc23200cf
AnkitKumar <ankit.kumar@infostride.com>
2022-01-13 19:57:48 +0530

    Demo on searching something on google.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,14 +1,28 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.concurrent.TimeUnit;
+
 
 public class Test {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.setProperty("webdriver.chrome.driver", "D:\\SeleniumProject\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://dev.bravvox.com/");
+        driver.get("https://www.google.com/");
         System.out.println(driver.getTitle());
-        driver.quit();
+        WebElement p=driver.findElement(By.name("q"));
+        p.sendKeys("facebook");
+        WebDriverWait w = new WebDriverWait(driver, 5);
+        w.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ul")));
+        p.submit();
+
+        // Thread.sleep(3000);
+        driver.close();
 
 
 
