76101778ac906d4bcbb8cc49956e046540e3470f
AnkitKumar <ankit.kumar@infostride.com>
2022-01-17 20:41:01 +0530

    Adding user on OrangeHRM website.

--- src/test/java/basic/HRM.java
+++ src/test/java/basic/HRM.java
@@ -5,8 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class HRM {
-        public static void main(String[] args) {
+        public static void main(String[] args) throws Exception {
             //WebDriverManager.chromedriver().setup();
             System.setProperty("webdriver.chrome.driver", "D:\\SeleniumProject\\chromedriver.exe");
             WebDriver driver=new ChromeDriver();
@@ -22,19 +24,19 @@ public class HRM {
             driver.findElement(By.xpath("//*[@id=\"menu_admin_viewAdminModule\"]/b")).click();
             driver.findElement(By.xpath("//*[@id=\"btnAdd\"]")).click();
 
-            driver.findElement(By.id("systemUser_employeeName")).sendKeys();
-            driver.findElement(By.xpath("//*[@id=\"systemUser_employeeName_empName\"]")).sendKeys("Aaliyah Haq");
-            driver.findElement(By.id("systemUser_userName")).sendKeys("Ankit");
-            driver.findElement(By.id("systemUser_password")).sendKeys("Ankit1234");
-            driver.findElement(By.id("systemUser_confirmPassword")).sendKeys("Ankit1234");
-
+            driver.findElement(By.id("systemUser_employeeName_empName")).sendKeys("Admin A");
+            //driver.findElement(By.xpath("//*[@id=\"systemUser_employeeName_empName\"]")).sendKeys("Aaliyah Haq");
+            driver.findElement(By.id("systemUser_userName")).sendKeys("Ankit51");
+            driver.findElement(By.id("systemUser_password")).sendKeys("Ankit123");
+            driver.findElement(By.id("systemUser_confirmPassword")).sendKeys("Ankit123");
+            driver.findElement(By.xpath("//*[@id=\"btnSave\"]")).click();
             if (expected_title.equals(actual_title)){
                 System.out.println("Test Passed");
             }else{
                 System.out.println("Test Failed");
             }
-
-            driver.close();
+            Thread.sleep(5000);
+            driver.quit();
         }
 
     }
