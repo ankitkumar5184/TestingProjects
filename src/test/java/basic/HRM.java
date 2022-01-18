811f8c4adc8b040e567a50abbfcfef47af6163b5
AnkitKumar <ankit.kumar@infostride.com>
2022-01-18 20:56:23 +0530

    Adding user on OrangeHRM website.

--- src/test/java/basic/HRM.java
+++ src/test/java/basic/HRM.java
@@ -1,16 +1,17 @@
 package basic;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
-
 public class HRM {
         public static void main(String[] args) throws Exception {
             //WebDriverManager.chromedriver().setup();
-            System.setProperty("webdriver.chrome.driver", "D:\\SeleniumProject\\chromedriver.exe");
+            WebDriverManager.chromedriver().setup();
+
             WebDriver driver=new ChromeDriver();
             driver.manage().window().maximize();
             driver.get("https://opensource-demo.orangehrmlive.com/");
@@ -19,7 +20,7 @@ public class HRM {
             Button=driver.findElement(By.id("txtPassword"));
             Button.sendKeys("admin123");
             driver.findElement(By.xpath("//*[@id=\"btnLogin\"]")).click();
-            String expected_title= "OrangeHRM";
+            System.out.println(driver.getTitle());
             String actual_title=driver.getTitle();
             driver.findElement(By.xpath("//*[@id=\"menu_admin_viewAdminModule\"]/b")).click();
             driver.findElement(By.xpath("//*[@id=\"btnAdd\"]")).click();
@@ -30,13 +31,29 @@ public class HRM {
             driver.findElement(By.id("systemUser_password")).sendKeys("Ankit123");
             driver.findElement(By.id("systemUser_confirmPassword")).sendKeys("Ankit123");
             driver.findElement(By.xpath("//*[@id=\"btnSave\"]")).click();
-            if (expected_title.equals(actual_title)){
+
+            ///driver.findElement(By.name("searchSystemUser[userName]")).sendKeys("Ankit51");
+            ///driver.findElement(By.id("searchBtn")).click();
+            ///String searchedUsernameXpath = "//a[Contains(text(),'%s')]";
+            ///WebElement searchedUsername = driver.findElement(By.xpath(String.format(searchedUsernameXpath,"Ankit51")));
+            ///Assert.isTrue(searchedUsername.getText().equals("Ankit51"),"user not found");
+
+            Assert.assertEquals(driver.getTitle(),actual_title);
+
+
+
+            if (driver.getTitle().equals(actual_title))
+            {
                 System.out.println("Test Passed");
-            }else{
+            }
+
+            else
+            {
                 System.out.println("Test Failed");
             }
+
             Thread.sleep(5000);
-            driver.quit();
+            driver.close();
         }
 
     }
