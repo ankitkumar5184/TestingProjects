f04572a4ea8e4a17ae61e6f8889d50fbd3521b7e
AnkitKumar <ankit.kumar@infostride.com>
2022-01-19 14:48:26 +0530

    Selenium datepicker from calender.

---
+++ src/test/java/basic/ClassName.java
@@ -0,0 +1,34 @@
+package basic;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ClassName {
+    @SuppressWarnings("deprecation")
+    public void Date() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.guru99.com/test/");
+        //noinspection deprecation
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement datebox = driver.findElement(By.xpath("//form//input[@name='bdaytime']"));
+        datebox.sendKeys("25092013");
+        datebox.sendKeys(Keys.TAB);
+        datebox.sendKeys("0245PM");
+        driver.findElement(By.xpath("/html/body/form/input[2]")).click();
+        driver.close();
+
+    }
+
+    public static void main(String[] args) {
+        ClassName cs = new ClassName();
+        cs.Date();
+    }
+}
